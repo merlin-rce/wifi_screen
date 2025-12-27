745c5caadd333ce749e055a02e0a18e7bb7aa798
Merlin Racine <merlin.racine@rpn.ch>
2025-12-27 23:29:05 +0100

    adding an exemple file

--- src/exemple.cpp
+++ src/exemple.cpp
@@ -1,11 +1,21 @@
+// Esp32 S3 and two I2C buses with Adafruit SSD1306 library (Two OLED 128x64 displays)
+// Forum: https://forum.arduino.cc/t/esp32-s3-and-two-i2c-buses-with-adafruit-ssd1306-library-two-oled-128x64-displays/1290094
+// Sketch by user: marko-2347
+// This Wokwi project: https://wokwi.com/projects/405730495697247233
+
+
+// OLED
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 //Define pins
-#define I2C_SDA 17 
-#define I2C_SCK 18 
+#define I2C_A_SDA 8 // GPIO8
+#define I2C_A_SCL 9 // GPIO9
+
+#define I2C_B_SDA 17 // GPIO17
+#define I2C_B_SCL 18 // GPIO18
 
 // OLED parameters
 #define SCREEN_WIDTH 128     // OLED display width, in pixels
@@ -16,21 +26,32 @@
 
 // Define display object
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display2(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
 
 void setup() {
   Serial.begin(115200);
   while (!Serial)
     ;
-   Wire.begin(I2C_SDA, I2C_SCK);
+   Wire.begin(I2C_A_SDA, I2C_A_SCL);
+   Wire1.begin(I2C_B_SDA, I2C_B_SCL);
+
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("Display1 SSD1306 allocation failed"));
     for (;;)
       ;  // Don't proceed, loop forever
   }
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display2.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("Display2 SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
 
 // Show splash screen
   display.display();
+  display2.display();
   delay(2000); // Pause for 2 seconds
 
   // Display settings
@@ -40,10 +61,18 @@ void setup() {
   display.setCursor(0,0);             // Start at top-left corner
   display.setRotation(ROTATION);      // Set screen rotation
 
+  // Display settings
+  display2.clearDisplay();
+  display2.setTextSize(2);             // Normal 1:1 pixel scale
+  display2.setTextColor(WHITE);        // Draw white text
+  display2.setCursor(0,0);             // Start at top-left corner
+  display2.setRotation(ROTATION);      // Set screen rotation
 
-  display.print(" nigga ");
+  display.print("Display1 ");
+  display2.print("Display2 nigga");
 
   display.display();
+  display2.display();
 
 }
 
